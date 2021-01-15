dde89b9a96878b0880c094d78a55c941ebe29258
Renato Augusto <renatoaugusto.ads@gmail.com>
2021-01-15 08:53:59 -0300

    Aprofundando no módulo 4 sobre métodos onde iremos tratar sobre os métodos static

---
+++ 04-Mais-sobre-metodos/src/Conta.php
@@ -0,0 +1,83 @@
+<?php
+
+class Conta
+{
+    private $cpfTitular;
+    private $nomeTitular;
+    private $saldo;
+    private static $numeroDeContas = 0;
+
+    public function __construct(string $cpfTitular, string $nomeTitular)
+    {
+        $this->cpfTitular = $cpfTitular;
+        $this->validaNomeTitular($nomeTitular);
+        $this->nomeTitular = $nomeTitular;
+        $this->saldo = 0;
+
+        self::$numeroDeContas++;
+    }
+
+    public function __destruct()
+    {
+        self::$numeroDeContas--;
+    }
+
+    public function saca(float $valorASacar): void
+    {
+        if ($valorASacar > $this->saldo) {
+            echo "Saldo indisponível";
+            return;
+        }
+
+        $this->saldo -= $valorASacar;
+    }
+
+    public function deposita(float $valorADepositar): void
+    {
+        if ($valorADepositar < 0) {
+            echo "Valor precisa ser positivo";
+            return;
+        }
+
+        $this->saldo += $valorADepositar;
+    }
+
+    public function transfere(float $valorATransferir, Conta $contaDestino): void
+    {
+        if ($valorATransferir > $this->saldo) {
+            echo "Saldo indisponível";
+            return;
+        }
+
+        $this->sacar($valorATransferir);
+        $contaDestino->depositar($valorATransferir);
+    }
+
+    public function recuperaSaldo(): float
+    {
+        return $this->saldo;
+    }
+
+    public function recuperaCpfTitular(): string
+    {
+        return $this->cpfTitular;
+    }
+
+    public function recuperaNomeTitular(): string
+    {
+        return $this->nomeTitular;
+    }
+
+    private function validaNomeTitular(string $nomeTitular)
+    {
+        if (strlen($nomeTitular) < 5) {
+            echo "Nome precisa ter pelo menos 5 caracteres";
+            exit();
+        }
+    }
+
+    public static function recuperaNumeroDeContas(): int
+    {
+        return self::$numeroDeContas;
+    }
+}
